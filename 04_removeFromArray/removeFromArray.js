3bb79b379b479cc35eb3911c1097f9ced9561023
Lori <loriwaltenbury@gmail.com>
2022-06-17 15:53:21 -0400

    finished #4;

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,42 @@
-const removeFromArray = function() {
+let remove;
+let array = new Array();
 
+const removeFromArray = function(array, remove) {
+
+    //for loop for each argument
+    
+        for (i = 1; i < arguments.length; i++){
+    
+    
+    
+            //take this into the for loop, run through array 1 by 1 ::: dont use i
+            for (j = 0; j < array.length; j++){
+    
+    
+                //if array[j] match argument[i]
+                if (array[j] === arguments[i]){
+    
+                    console.log(array[j]);
+    
+                    //remove
+                    array.splice(j,1);
+    
+                }
+    
+            }
+    
+    
+        }
+
+        
+    if (array.length < 1){
+        return array = new Array ();
+    
+    } else {
+        return array;
+    }
+    
+    
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,45 @@
-const sumAll = function() {
+let first;
+let second;
+let smaller;
+let larger;
+let sum = 0;
 
-};
+const sumAll = function(first, second) {
+    
+sum = 0;
 
+//check which number is smaller
+
+if (first < second){
+    smaller = first;
+    larger = second;
+} else if (second < first){
+    smaller = second;
+    larger = first;
+}
+
+
+// if numbers are equal or separate by 1
+
+if (smaller < 1 || larger < 1){
+    return "ERROR";
+} else if (typeof smaller != "number" || typeof larger != "number"){
+    return "ERROR";
+} else if (smaller == larger || (larger - smaller) == 1){
+
+    sum = smaller + larger;
+
+} else {
+
+for (i = smaller; i <= larger; i++){
+
+    sum +=i;
+
+}
+}
+
+return sum;
+
+}
 // Do not edit below this line
 module.exports = sumAll;
