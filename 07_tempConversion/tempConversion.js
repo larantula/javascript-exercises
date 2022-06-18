25494ff7a2b456e210eca6f1172f6d146da0023a
Lori <loriwaltenbury@gmail.com>
2022-06-17 22:18:50 -0400

    finished #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,26 @@
-const ftoc = function() {
+let f;
+let c;
+
+//c = (f - 32) / 1.8000;
+//f = (c*1.8000) + 32;
+
+const ftoc = function(f) {
+
+    c = (f - 32) / 1.8000;
+    c = Math.round(c * 10) / 10;
+    return c;
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+
+    f = (c * 1.8000) + 32;
+    f = Math.round(f * 10) / 10;
+    return f;
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = {
